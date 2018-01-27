341e2b7ab7ce1b10a38b02799c2cb5534b0d2e63
apelofff <joergenstolt@gmail.com>
2018-01-27 01:10:05 +0100

    j_scripts: mappe for Jørgen sine scripts
    Cam_follow: Regner ut avstanden mellom spillerene, og holder midtpunktet.
    
    PlayerMovement: Lerper spillerene mot hverandre.

---
+++ Assets/GlobalGameJam/j_Scripts/Cam_follow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam_follow : MonoBehaviour
+{
+
+    public GameObject Player1;
+    public GameObject Player2;
+
+    [Range(-5, -20)] public float distance;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3((Player1.transform.position.x + Player2.transform.position.x) / 2, 0, distance);
+        /*distance = middle.x * 2;
+        transform.position = middle +  new Vector3(0,0,distance);*/
+
+
+
+    }
+}
---
+++ Assets/GlobalGameJam/j_Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Transform otherPlayer;
+    [Header("Tweak")]
+    public float attraction = 0.1f;
+    public float distance;
+    [Range(0.004f, 1)] public float attractionMulitplier;
+    [Range(1f, 10f)] public float thrust;
+
+    public Rigidbody rb1;
+    
+
+    private void Start()
+    {
+        rb1 = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        distance = transform.position.x - otherPlayer.transform.position.x;
+        transform.position = Vector3.Lerp(transform.position, otherPlayer.position, attractionMulitplier);
+
+        if (Input.GetButton("Mouse0"))
+        {
+            GetComponent<Rigidbody>().AddForce(transform.forward * thrust);
+        }
+
+
+    }
+
+}
