bf0fb02a1130fdff1db77a78448a7f4ae7275e48
apelofff <joergenstolt@gmail.com>
2018-01-28 05:32:34 +0100

    uferdig camerashake. Buggy. Disable script fram til eg sier ifra.

---
+++ Assets/GlobalGameJam/CamSheik.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSheik : MonoBehaviour {
+    public float power;
+    public float duration;
+    public Transform camera;
+    public float slowDownTime;
+    public bool shouldShake = false;
+    Vector3 startingPos;
+    public float initialDuration;
+
+	// Use this for initialization
+	void Start ()
+    {
+        camera = Camera.main.transform;
+
+        initialDuration = duration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (shouldShake)
+        {
+            if (duration < 0)
+            {
+                camera.localPosition = startingPos + Random.insideUnitSphere * power;
+                duration -= Time.deltaTime * slowDownTime;
+              
+            }
+            else
+            {
+                shouldShake = false;
+                duration = initialDuration;
+                camera.localPosition = startingPos;
+            }
+        }
+	}
+}
